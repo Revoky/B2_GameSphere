fa501bc2689ae71918f330add3ce5de8014550fb
Revoky <fannycr1708@gmail.com>
2024-04-26 11:27:31 +0200

    base

---
+++ back/database.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conn = sqlite3.connect('game_sphere.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS JEUX (
+        id INTEGER PRIMARY KEY,
+        image TEXT,
+        nom TEXT NOT NULL,
+        prix REAL NOT NULL,
+        note_moyenne REAL,
+        avis_utilisateur TEXT,
+        quantite INTEGER
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS UTILISATEURS (
+        id INTEGER PRIMARY KEY,
+        prenom TEXT,
+        nom TEXT,
+        mot_de_passe TEXT NOT NULL,
+        mail TEXT UNIQUE NOT NULL,
+        date_naissance TEXT,
+        solde REAL DEFAULT 0,
+        num_facture INTEGER
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS FACTURES (
+        id INTEGER PRIMARY KEY,
+        id_utilisateur INTEGER,
+        nom_jeu TEXT,
+        prix_jeu REAL,
+        ancien_solde REAL,
+        nouveau_solde REAL,
+        date_facture TEXT,
+        FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEURS(id)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS ADMINS (
+        id INTEGER PRIMARY KEY,
+        nom_utilisateur TEXT UNIQUE NOT NULL,
+        mot_de_passe TEXT NOT NULL
+    )
+''')
+
+conn.commit()
+conn.close()
