e61d83861c4d414c77e370209e6a55cb4ea99700
slea <lea.socolovert@ynov.com>
2024-05-17 09:29:28 +0200

    update tables

--- Database/database.py
+++ Database/database.py
@@ -18,6 +18,7 @@ cursor.execute('''
         prenom TEXT,
         nom TEXT,
         mot_de_passe TEXT NOT NULL,
+        image TEXT,
         mail TEXT UNIQUE NOT NULL,
         date_naissance TEXT,
         solde REAL DEFAULT 0,
@@ -32,32 +33,55 @@ cursor.execute('''
         nom TEXT NOT NULL,
         prix REAL NOT NULL,
         note_moyenne REAL,
-        avis_utilisateur TEXT,
         quantite INTEGER
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS AVIS (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        game_id INTEGER,
+        body TEXT,
+        note INTEGER,
+        FOREIGN KEY(user_id) REFERENCES UTILISATEURS(id),
+        FOREIGN KEY(game_id) REFERENCES JEUX(id)
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS FACTURES (
         id INTEGER PRIMARY KEY,
-        id_utilisateur INTEGER,
+        id_game_user INTEGER,
         nom_jeu TEXT,
         prix_jeu REAL,
         ancien_solde REAL,
         nouveau_solde REAL,
         date_facture TEXT,
-        FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEURS(id)
+        FOREIGN KEY (id_game_user) REFERENCES JEUX_USERS(id),
     )
 ''')
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS JEUX_USERS (
+        id INTEGER PRIMARY KEY,
         user_id INTEGER,
         game_id INTEGER,
         FOREIGN KEY(user_id) REFERENCES UTILISATEURS(id)
         FOREIGN KEY(game_id) REFERENCES JEUX(id)
     )
 ''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS JEUX_AVIS (
+        id INTEGER PRIMARY KEY,
+        avis_id INTEGER,
+        game_id INTEGER,
+        FOREIGN KEY(avis_id) REFERENCES AVIS(id)
+        FOREIGN KEY(game_id) REFERENCES JEUX(id)
+    )
+''')
+
 
 connection.commit()
 connection.close()
