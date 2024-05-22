3befc12378f2bb3b1fbbb1e7d528c44f6cdcaa9c
slea <lea.socolovert@ynov.com>
2024-05-22 17:52:53 +0200

    fix

--- Database/API.py
+++ Database/API.py
@@ -80,6 +80,8 @@ def get_users():
 def create_user():
     data = request.json
     try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
         cursor.execute('''INSERT INTO UTILISATEURS (prenom, nom, mot_de_passe, mail, date_naissance) VALUES (?, ?, ?, ?, ?)''', 
             (data['prenom'], data['nom'], data['mot_de_passe'], data['mail'], data['date_naissance']))
         conn.commit()
@@ -93,6 +95,8 @@ def create_user():
 def update_user(id):
     data = request.json
     try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
         cursor.execute('''UPDATE UTILISATEURS SET prenom=?, nom=?, mot_de_passe=?, mail=?, date_naissance=? WHERE id=?''', 
             (data['prenom'], data['nom'], data['mot_de_passe'], data['mail'], data['date_naissance'], id))
         conn.commit()
