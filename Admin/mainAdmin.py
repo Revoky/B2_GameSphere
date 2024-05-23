ae112a67e49a05d6cf28cbfed9ef4b3f13c330e2
slea <lea.socolovert@ynov.com>
2024-05-23 22:31:43 +0200

    add user and game via admin

--- Admin/mainAdmin.py
+++ Admin/mainAdmin.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, session, g, jsonify
+from flask import Flask, render_template, request, redirect, session, g, url_for
 import sqlite3
+
+from Database.new import new_jeux, new_utilisateur
 
 app = Flask(__name__)
 app.secret_key = '7s3p3uBZ'
@@ -93,7 +95,7 @@ def get_games_api():
     except Exception as e:
         return {'error': str(e)}, 500
 
-@app.route('/admin/index')
+@app.route('/admin/index', methods=['GET', 'POST'])
 def admin_index():
     if 'admin_logged_in' in session and session['admin_logged_in']:
         users = get_users_api()
@@ -101,6 +103,43 @@ def admin_index():
         return render_template('index.html', users=users, games=games)
     else:
         return redirect('/admin/login')
+
+@app.route('/admin/new_utilisateur', methods=['GET', 'POST'])
+def add_utilisateur():
+    if request.method == 'POST':
+        prenom = request.form['prenom']
+        nom = request.form['nom']
+        mot_de_passe = request.form['mot_de_passe']
+        image = request.form['image']
+        mail = request.form['mail']
+        date_naissance = request.form['date_naissance']
+        solde = request.form['solde']
+        new_utilisateur(prenom, nom, mot_de_passe, image, mail, date_naissance, float(solde))
+        return redirect('/admin/index')
+    return render_template('new_utilisateur.html')
+
+@app.route('/admin/new_jeux', methods=['GET', 'POST'])
+def add_jeux():
+    if request.method == 'POST':
+        image = request.form['image']
+        nom = request.form['nom']
+        prix = request.form['prix']
+        note_moyenne = request.form['note_moyenne']
+        avis_utilisateur = request.form['avis_utilisateur']
+        quantite = request.form['quantite']
+
+        # Print out the SQL query and its parameters
+        print("SQL Query:")
+        print('''
+                   INSERT INTO JEUX (image, nom, prix, note_moyenne, avis_utilisateur, quantite)
+                   VALUES (?, ?, ?, ?, ?, ?)
+               ''')
+        print("Parameters:")
+        print((image, nom, prix, note_moyenne, avis_utilisateur, quantite))
+
+        new_jeux(image, nom, float(prix), float(note_moyenne) if note_moyenne else None, avis_utilisateur if avis_utilisateur else None, int(quantite))
+        return redirect('/admin/index')
+    return render_template('new_jeux.html')
 
 @app.route('/admin/logout')
 def logout():
